c06e2664e432179f355f87e36b07f3540f0090b9
Aimery Pineau <aimery.pin@gmail.com>
2023-04-05 23:43:04 +0200

    Twig-1

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 
 // Get a $twig object from this file.
-require_once __DIR__ . '/../config/twig.php';
+require __DIR__ . '/../config/twig.php';
 
 
 $name = 'Wilder';
---
+++ public/products.php
@@ -0,0 +1,11 @@
+<?php
+
+
+
+
+require __DIR__ . '/../config/twig.php';
+
+$products = ['guitare', 'bass', 'bonjo', 'cithare', 'lyre', 'harpe'];
+
+echo $twig->render('products.html.twig', ['products' => $products]);
+
